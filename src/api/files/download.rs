06706fb3aa89b005bdb96d1cce5d70e1ebfd79d4
KongouDesu <kongou@shipfu.moe>
2018-08-16 18:50:37 +0200

    Added download_file_by_id

--- src/api/files/download.rs
+++ src/api/files/download.rs
@@ -14,6 +14,7 @@ use handle_b2error_kinds;
 use api::files::structs::*;
 
 /// Download a file by it's assigned id
+///
 /// Returns a struct containing the file info and a string containing the raw file
 ///
 /// Equivalent to b2_download_file_by_id - [Official documentation](https://www.backblaze.com/b2/docs/b2_download_file_by_id.html)
@@ -22,7 +23,56 @@ pub fn download_file_by_id(client: &reqwest::Client, auth: &B2Auth, file_id: &st
     // The auth token from an UploadAuth, see: get_upload_url
     headers.set(reqwest::header::Authorization(auth.authorization_token.clone()));
     // Send the request
-    let mut resp = match client.get(&format!("{}//b2api/v1/b2_download_file_by_id?fileId={}",auth.download_url,file_id))
+    let mut resp = match client.get(&format!("{}/b2api/v1/b2_download_file_by_id?fileId={}",auth.download_url,file_id))
+        .headers(headers)
+        .send() {
+        Ok(v) => v,
+        Err(e) => return Err(B2Error::ReqwestError(e))
+    };
+    // Read the response to a string containing the JSON response
+    let response_string = resp.text().unwrap();
+    // Test if the response body is an error, if not, the body will be the file and we continue to extract headers
+    use B2ApiError;
+    use B2Error;
+    match serde_json::from_str::<B2ApiError>(&response_string) {
+        Ok(v) => return Err(B2Error::B2Error(v)),
+        Err(_e) => (),
+    };
+    let headers = resp.headers();
+    use reqwest::header::*;
+    let len = match headers.get::<ContentLength>() {
+        Some(t) => Some(t.0),
+        None => None,
+    };
+    let content_type = match headers.get::<ContentType>() {
+        Some(t) => Some(format!("{}",t)),
+        None => None,
+    };
+    let fileinfo = FileInfo {
+        content_length: len,
+        content_type: content_type,
+        x_bz_content_sha1: raw_header_to_str(headers,"x-bz-content-sha1"),
+        x_bz_file_id: raw_header_to_str(headers,"x-bz-file-id"),
+        x_bz_file_name: raw_header_to_str(headers,"x-bz-file-name"),
+        x_bz_info_src_last_modified_millis: raw_header_to_u64(headers,"x-bz-info-src_last_modified_millis"),
+        x_bz_upload_timestamp: raw_header_to_u64(headers, "X-Bz-Upload-Timestamp"),
+    };
+    Ok((fileinfo, response_string))
+}
+
+/// Download a file by it's given name
+///
+/// Returns a struct containing the file info and a string containing the raw file
+///
+/// The file name should be the full name on the server, eg. if you're emulating folders you should use the full path
+///
+/// Equivalent to b2_download_file_by_name - [Official documentation](https://www.backblaze.com/b2/docs/b2_download_file_by_name.html)
+pub fn download_file_by_name(client: &reqwest::Client, auth: &B2Auth, bucket_name: &str, file_name: &str) -> Result<(FileInfo,String), B2Error> {
+    let mut headers = reqwest::header::Headers::new();
+    // The auth token from an UploadAuth, see: get_upload_url
+    headers.set(reqwest::header::Authorization(auth.authorization_token.clone()));
+    // Send the request
+    let mut resp = match client.get(&format!("{}/file/{}/{}",auth.download_url,bucket_name, file_name))
         .headers(headers)
         .send() {
         Ok(v) => v,
@@ -105,7 +155,6 @@ mod tests {
 
     // Tests that we can upload a file, fails if get_upload_url fails
     // Tests that we can delete a file, won't be tested if uploading fails
-    // TODO This leaves a mess if upload works but delete doesn't
     #[test]
     #[allow(unused_variables)]
     fn test_download_file() {
@@ -114,15 +163,19 @@ mod tests {
         let res = misc::get_upload_url(&client, &autho, TEST_BUCKET_ID).unwrap();
         let n = upload_file(&client, &res, std::path::Path::new("./testfile.txt"), "some_folder").unwrap();
 
-        let h = download::download_file_by_id(&client, &autho, &n.file_id);
+        let by_id = download::download_file_by_id(&client, &autho, &n.file_id);
+        let by_name = download::download_file_by_name(&client, &autho, "uploaded-files", "some_folder/testfile.txt");
         // Delete file before assertions to prevent leaving files around
         let x = misc::delete_file_version(&client, &autho, n.file_name, n.file_id);
 
-        match h {
+        match by_id {
             Ok(_t) => (),
             Err(_e) => assert!(false),
         }
 
-
+        match by_name {
+            Ok(_t) => (),
+            Err(_e) => assert!(false),
+        }
     }
 }
